1f1874f97654f9856e0907d76e75ea3a04886d2b
zhanggen <gen.zhang@yl.com>
2026-02-24 11:03:34 +0800

    Initial commit for iOS-Evaluate Swift Package

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "iOS-Evaluate",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Evaluate",
+            targets: ["Evaluate"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Evaluate",
+            path: "Sources/iOS-Evaluate",
+            resources: [
+                .process("Resources")
+            ]
+        ),
+        .testTarget(
+            name: "EvaluateTests",
+            dependencies: ["Evaluate"]),
+    ]
+)
