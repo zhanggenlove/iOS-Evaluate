83c92d0101b101a3a3c25ba7bd269a563f6aa1fa
zhanggen <gen.zhang@yl.com>
2026-02-24 13:59:08 +0800

    fix: Use SPM Bundle.module for localization

--- Sources/iOS-Evaluate/Extensions/String+Extensions.swift
+++ Sources/iOS-Evaluate/Extensions/String+Extensions.swift
@@ -9,10 +9,20 @@ import Foundation
 
 extension String {
   static func localize(_ key: String) -> String {
-    return NSLocalizedString(key, tableName: "EvaluateLocalization", bundle: Bundle(for: Evaluate.self), comment: "")
+#if SWIFT_PACKAGE
+    let bundle = Bundle.module
+#else
+    let bundle = Bundle(for: Evaluate.self)
+#endif
+    return NSLocalizedString(key, tableName: "EvaluateLocalization", bundle: bundle, comment: "")
   }
   
   var localize: String {
-    return NSLocalizedString(self, tableName: "EvaluateLocalization", bundle: Bundle(for: Evaluate.self), comment: "")
+#if SWIFT_PACKAGE
+    let bundle = Bundle.module
+#else
+    let bundle = Bundle(for: Evaluate.self)
+#endif
+    return NSLocalizedString(self, tableName: "EvaluateLocalization", bundle: bundle, comment: "")
   }
 }
